b13db78170a7bfdeb8b73a7ce16cf67148ecbfbd
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-19 21:28:50 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function Phone(brand, price, color) {
 }
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". ");
-}
+};
 var iPhone6S = new Phone("Apple", 2250, "silver");
 var SamsungGalaxyS6 = new Phone("Samsung Galaxy", 1500, "black");
 var OnePlusOne = new Phone("OnePlus One", 250, "white");
