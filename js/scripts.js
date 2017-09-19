3ba50866df9369a58faf6e04ab113515040525bf
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-19 19:38:39 +0200

    Zadanie 11.4

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+// Excercise 11.4
+
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". ");
+}
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var SamsungGalaxyS6 = new Phone("Samsung Galaxy", 1500, "black");
+var OnePlusOne = new Phone("OnePlus One", 250, "white");
+
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
